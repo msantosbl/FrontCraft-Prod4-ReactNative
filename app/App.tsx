b4e34cae40880f256eaad9d8793be7a214bfde78
GayLachner-Marc <mgayl@uoc.edu>
2024-12-13 17:14:44 +0100

    Principios de instalación de yarn/app + creacion de las notificaciones/messaging en el doc app.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import OurTabs from '@/components/TabBar/Navigation';
 import {PlayersComponent} from "@/components/PlayerCrudComponent/CrudComponent"; // Ruta correcta
-
+import messaging from '@react-native-firebase/messaging';
 const App = () => {
+ async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  const getToken = async() => {
+    const token = await messaging().getToken()
+    console.log ("Token = ", token)
+  }
+  useEffect(()=>{
+    requestUserPermission()
+    getToken()
+  })
   return (
     <NavigationContainer>
       <OurTabs /> {/* Aquí va tu Tab Navigator */}
